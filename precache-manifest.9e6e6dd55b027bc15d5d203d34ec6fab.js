676ece5fe6c4272a5655018977fcc5e253001b34
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-10-13 03:40:21 -0400

    Updates

---
+++ precache-manifest.9e6e6dd55b027bc15d5d203d34ec6fab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f1557f792fc027f76d6",
+    "url": "/clicker-game/static/js/runtime~main.5f1557f7.js"
+  },
+  {
+    "revision": "cf9f0eb165bbed64315c",
+    "url": "/clicker-game/static/js/main.cf9f0eb1.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/clicker-game/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "cf9f0eb165bbed64315c",
+    "url": "/clicker-game/static/css/main.6edcb829.chunk.css"
+  },
+  {
+    "revision": "695371e3c45769c75e0e483440be9da9",
+    "url": "/clicker-game/index.html"
+  }
+];
